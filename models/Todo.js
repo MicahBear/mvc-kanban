8a261ed8bf1faedf541de98e8021783544936887
MicahBear <micah.b.elm@gmail.com>
2022-09-02 18:29:58 -0700

    updating with team changes

--- models/Todo.js
+++ models/Todo.js
@@ -1,13 +1,39 @@
+// const mongoose = require('mongoose')
+
+// const TodoSchema = new mongoose.Schema({
+//   todo: {
+//     type: String,
+//     required: true,
+//   },
+//   completed: {
+//     type: Boolean,
+//     required: true,
+//   },
+//   userId: {
+//     type: String,
+//     required: true
+//   }
+// })
+
+// module.exports = mongoose.model('Todo', TodoSchema)
+
+
 const mongoose = require('mongoose')
 
 const TodoSchema = new mongoose.Schema({
-  todo: {
+  title: {
     type: String,
     required: true,
   },
-  completed: {
-    type: Boolean,
+  description: {
+    type: String,
+    required: false,
+  },
+  status: {
+    type: String,
     required: true,
+    default: 'progress',
+    enum: ['progress', 'completed']
   },
   userId: {
     type: String,
--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,126 @@
+// const deleteBtn = document.querySelectorAll('.del')
+// const todoItem = document.querySelectorAll('span.not')
+// const todoComplete = document.querySelectorAll('span.completed')
+// const todoList = document.getElementById('todo-board')
+// const progessList = document.getElementById('progress-board')
+// const completedList = document.getElementById('completed-board')
+
+// Array.from(deleteBtn).forEach((el) => {
+//     el.addEventListener('click', deleteTodo)
+// })
+
+// Array.from(todoItem).forEach((el) => {
+//     el.addEventListener('click', markComplete)
+// })
+
+// Array.from(todoComplete).forEach((el) => {
+//     el.addEventListener('click', markIncomplete)
+// })
+
+// async function deleteTodo() {
+//     const todoId = this.parentNode.dataset.id
+//     try {
+//         const response = await fetch('todos/deleteTodo', {
+//             method: 'delete',
+//             headers: { 'Content-type': 'application/json' },
+//             body: JSON.stringify({
+//                 'todoIdFromJSFile': todoId
+//             })
+//         })
+//         const data = await response.json()
+//         console.log(data)
+//         location.reload()
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }
+
+// async function markComplete() {
+//     const todoId = this.parentNode.dataset.id
+//     try {
+//         const response = await fetch('todos/markComplete', {
+//             method: 'put',
+//             headers: { 'Content-type': 'application/json' },
+//             body: JSON.stringify({
+//                 'todoIdFromJSFile': todoId
+//             })
+//         })
+//         const data = await response.json()
+
+
+//         console.log(data)
+//         location.reload()
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }
+
+// async function markIncomplete() {
+//     const todoId = this.parentNode.dataset.id
+//     try {
+//         const response = await fetch('todos/markIncomplete', {
+//             method: 'put',
+//             headers: { 'Content-type': 'application/json' },
+//             body: JSON.stringify({
+//                 'todoIdFromJSFile': todoId
+//             })
+//         })
+//         const data = await response.json()
+//         console.log(data)
+//         location.reload()
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }
+
+function init() {
+    return dragula([
+        document.getElementById('uL1'), //parent container:todo
+        document.getElementById('uL2'), //parent container: inprogress
+        document.getElementById('uL3'), // parent container: completed
+        //document.getElementById('4'),
+        //document.getElementById('5')
+    ])
+}
+
+const btn = document.getElementById('displayandhide').addEventListener('click', displayOrHide); // button to hide the input 
+
 const deleteBtn = document.querySelectorAll('.del')
 const todoItem = document.querySelectorAll('span.not')
 const todoComplete = document.querySelectorAll('span.completed')
+// const dragulaProgress = document.querySelectorAll('')
 
-Array.from(deleteBtn).forEach((el)=>{
+
+
+function displayOrHide() {    // function to hide inputs when adding a new task
+    const form = document.getElementById('form');
+    if (form.style.display === 'none') {
+        // 👇️ this SHOWS the form
+        form.style.display = 'block';
+    } else {
+        // 👇️ this HIDES the form
+        form.style.display = 'none';
+    }
+}
+
+Array.from(deleteBtn).forEach((el) => {
     el.addEventListener('click', deleteTodo)
 })
 
-Array.from(todoItem).forEach((el)=>{
+Array.from(todoItem).forEach((el) => {
     el.addEventListener('click', markComplete)
 })
 
-Array.from(todoComplete).forEach((el)=>{
+Array.from(todoComplete).forEach((el) => {
     el.addEventListener('click', markIncomplete)
 })
 
-async function deleteTodo(){
+async function deleteTodo() {
     const todoId = this.parentNode.dataset.id
-    try{
+    try {
         const response = await fetch('todos/deleteTodo', {
             method: 'delete',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
                 'todoIdFromJSFile': todoId
             })
@@ -27,17 +128,17 @@ async function deleteTodo(){
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
-async function markComplete(){
+async function markComplete() {
     const todoId = this.parentNode.dataset.id
-    try{
+    try {
         const response = await fetch('todos/markComplete', {
             method: 'put',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
                 'todoIdFromJSFile': todoId
             })
@@ -45,17 +146,17 @@ async function markComplete(){
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
-async function markIncomplete(){
+async function markIncomplete() {
     const todoId = this.parentNode.dataset.id
-    try{
+    try {
         const response = await fetch('todos/markIncomplete', {
             method: 'put',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
                 'todoIdFromJSFile': todoId
             })
@@ -63,7 +164,7 @@ async function markIncomplete(){
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
